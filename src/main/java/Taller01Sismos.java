eddef5a6a283fe285edc532f0608ed3aa05780dd
Pincheira07 <m.pincheira07@ufromail.cl>
2021-09-01 10:23:50 -0400

    Commit 2

--- src/main/java/Taller01Sismos.java
+++ src/main/java/Taller01Sismos.java
@@ -1,3 +1,6 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Taller01Sismos {
@@ -6,17 +9,18 @@ public class Taller01Sismos {
     }
 
     public static void iniciarSimulacion() {
-        iniciarMenu();
+        double[][] sismos = new double[10][7];
+        iniciarMenu(sismos);
     }
 
-    public static void iniciarMenu() {
+    public static void iniciarMenu(double[][] sismos) {
         while (true) {
             mostrarMenu();
             int opcion = leerOpcionMenu();
             if (!validarOpcionMenu(opcion)){
                 System.out.println("Valor fuera de rango");
             }else{
-                ejecutarMenu(opcion);
+                ejecutarMenu(opcion, sismos);
             }
         }
     }
@@ -44,25 +48,66 @@ public class Taller01Sismos {
 
     }
 
-    public static void ejecutarMenu(int option) {
+    public static void ejecutarMenu(int option, double[][] sismos) {
         switch (option) {
             case 1 -> {
                 System.out.println("Ingresando datos...");
-
+                llenarArreglo(sismos);
             }
             case 2 -> {
                 System.out.println("El sismo de mayor magnitud fue: ");
+                mostrarArreglo(sismos);
             }
             case 3 -> {
-                System.out.println("Hubieron " + 10 +"sismos mayores a 5.0");
+                int mayoresA5 = contarMayoresa5(sismos);
+                System.out.println("¡¡Hubieron " + mayoresA5 +" sismos de magnitud mayor a 5.0!!");
             }
             case 4 -> {
-                System.out.println("Alerta!!! se debe evacuar zona costera!");
-
+                enviarSMS(sismos);
             }
             case 0 -> System.exit(0);
         }
     }
 
+    public static double[][] llenarArreglo(double[][] sismos) {
+        for (int i = 0; i < sismos.length ; i++) {
+            for (int j = 0; j < sismos[i].length; j++) {
+                sismos[i][j] =  Math.random()*10;
+            }
+        }
+        return sismos;
+    }
+
+    public static void mostrarArreglo(double[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print("[" + arr[i][j] + "]");
+
+            }
+            System.out.println("");
+        }
+    }
+
+    public static int contarMayoresa5(double[][] arr) {
+        int contadorMayoresA5 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j]>=5.0){
+                    contadorMayoresA5++;
+                }
+            }
+        }
+        return contadorMayoresA5;
+    }
+
+    public static void enviarSMS(double[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j]>=7.0){
+                    System.out.println("Alerta!!! se debe evacuar zona costera!");
+                }
+            }
+        }
+    }
 
 }
